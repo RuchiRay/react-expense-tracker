e53e48444c098a09475505fe1639760cc8078f48
Ruchi Ray <ruchiray18@gmail.com>
2021-07-07 13:15:53 +0530

    added chart js

--- src/myComponents/Expense.js
+++ src/myComponents/Expense.js
@@ -1,10 +1,51 @@
-import React from 'react'
-import { useGlobalContext } from '../Context'
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { useGlobalContext } from "../Context";
 export const Expense = () => {
-    const {expense} = useGlobalContext();
-    return (
-        <div className='expense-box'>
-            expense ={expense}
-        </div>
-    )
-}
+  const { expense, expenseBox } = useGlobalContext();
+  const expenseArray = expenseBox.filter((expense) => {
+    return expense.amount > 0;
+  });
+  let chartLabel = [];
+  let chartDataSets = [];
+  let bgColors = [];
+  for (const exp of expenseArray) {
+    chartLabel = [...chartLabel, exp.type];
+    chartDataSets = [...chartDataSets, exp.amount];
+    bgColors = [...bgColors, exp.color];
+  }
+  const chartData = {
+    labels: chartLabel,
+    datasets: [
+      {
+        label: "Income",
+        backgroundColor: bgColors,
+        data: chartDataSets,
+        hoverOffset: 6,
+      },
+    ],
+  };
+
+  const chartOptions = {
+    plugins: {
+      title: {
+        display: true,
+        text: `$${expense}`,
+        font: {
+          size: 20,
+          weight: "normal",
+        },
+      },
+    },
+  };
+
+  return <div className="expense-box">
+      <h3>Expense</h3>
+      <div className="chart">
+      <Doughnut
+        data={chartData}
+        options={chartOptions}
+      />
+      </div>
+      </div>;
+};
--- src/myComponents/Income.js
+++ src/myComponents/Income.js
@@ -1,10 +1,56 @@
-import React from 'react'
-import { useGlobalContext } from '../Context'
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { useGlobalContext } from "../Context";
 export const Income = () => {
-    const {income} = useGlobalContext()
-    return (
-        <div className='income-box'>
-            income = {income}
-        </div>
-    )
-}
+  const { income, incomeBox, expenseBox } = useGlobalContext();
+  const incomeArray = incomeBox.filter((income)=>{
+    return income.amount>0
+})
+  let chartLabel =[]
+  let chartDataSets = [];
+  let bgColors = [];
+  for(const inc of incomeArray ){
+      chartLabel = [...chartLabel,inc.type];
+      chartDataSets = [...chartDataSets,inc.amount]
+      bgColors = [...bgColors,inc.color]
+  }
+  const chartData = {
+    labels: chartLabel,
+    datasets: [
+      {
+        label: "Income",
+        backgroundColor: bgColors,
+        data: chartDataSets,
+        hoverOffset: 6
+      },
+    ],
+  };
+
+  const chartOptions = {
+    plugins: {
+        title: {
+            display: true,
+            text: `$${income}`,
+            font:{
+                size:20,
+                weight:'normal'
+            }
+        },
+        
+    },
+   
+  }
+  console.log(chartOptions);
+  return (
+    <div className="income-box">
+      <h3>Income</h3>
+      <div className="chart">
+      <Doughnut
+        data={chartData}
+        options={chartOptions}
+      />
+      </div>
+      
+    </div>
+  );
+};
